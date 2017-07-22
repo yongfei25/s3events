8a05884704c99cefd3089b8800ec84d1cfa349f0
Yong Fei <yongfei@iflix.com>
2017-07-22 18:38:45 +0800

    Add notify-sqs command

--- commands/notify-sqs.ts
+++ commands/notify-sqs.ts
@@ -3,13 +3,13 @@ import * as yargs from 'yargs'
 import * as common from '../lib/common'
 import * as sender from '../lib/sender'
 
-exports.command = 'notify-sns <event> <topicArn> <s3Path>'
-exports.desc = 'Send event for each object in the path, to a SNS topic.'
+exports.command = 'notify-sqs <event> <queueArn> <s3Path>'
+exports.desc = 'Send event for each object in the path, to a SQS queue.'
 exports.builder = function (yargs:yargs.Argv) {
   yargs.demand(['event'])
     .choices('event', ['ObjectCreated:*', 'ObjectRemoved:*', 'ReducedRedundancyLostObject'])
     .describe('event', 'Event type')
-    .describe('topicArn', 'SNS topic ARN')
+    .describe('queueArn', 'SQS queue ARN')
     .describe('s3Path', 'Example: s3://prefix')
     .describe('suffix', 'S3 key suffix')
     .describe('dryrun', 'Do not send event')
@@ -19,17 +19,17 @@ exports.handler = async function (argv) {
   try {
     const s3Path = common.parseS3Path(argv.s3Path)
     const s3 = common.getS3()
-    const sns = common.getSNS()
+    const sqs = common.getSQS()
     const numObjects = await common.forEachS3KeyInPrefix(s3, s3Path.bucket, s3Path.key, async function (object:AWS.S3.Object) {
       let filterRules = []
       if (argv.suffix) {
         filterRules.push({ Name: 'Suffix', Value: argv.suffix })
       }
       if (argv.dryrun) {
-        console.log(`(dryrun) Send ${argv.event} to ${argv.topicArn} for ${object.Key}`)
+        console.log(`(dryrun) Send ${argv.event} to ${argv.queueArn} for ${object.Key}`)
       } else {
-        console.log(`Send ${argv.event} to ${argv.topicArn} for ${object.Key}`)
-        await sender.sendSNSNotification(sns, argv.topicArn, {
+        console.log(`Send ${argv.event} to ${argv.queueArn} for ${object.Key}`)
+        await sender.sendSQSMessage(sqs, argv.queueArn, {
           bucket: s3Path.bucket,
           eventName: argv.event,
           object: object,
